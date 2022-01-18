dc939f1b7c1ea1b4f0d29b78e92044dacfda5186
Bahattin <bahattincloud@gmail.com>
2022-01-19 00:37:05 +0300

    Last update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Initalization
 const express = require('express');
+const request = require('request');
 const config = require('./config.json'); // Website config
 const FormData = require('form-data');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
 app.use(require('express-session')(config.session))
 
@@ -12,6 +13,7 @@ app.get('/', async (req, resp) => {
     
     const data = await fetch(`https://discord.com/api/users/@me`, {headers: { Authorization: `Bearer ${req.session.bearer_token}` } }); // Fetching user data
     const json = await data.json();
+	console.log(json)
 
     if(!json.username) // This can happen if the Bearer token has expired or user has not given permission "indentity"
         return resp.redirect('/login') // Redirect to login page
@@ -31,12 +33,25 @@ app.get('/login/callback', async (req, resp) => {
     data.append('client_secret', config.oauth2.secret);
     data.append('grant_type', 'authorization_code');
     data.append('redirect_uri', config.oauth2.redirect_uri);
-    data.append('scope', 'identify');
+    data.append('scope', 'gdm.join');
     data.append('code', accessCode);
+	
 
     // Making request to oauth2/token to get the Bearer token
-    const json = await (await fetch('https://discord.com/api/oauth2/token', {method: 'POST', body: data})).json();
+    const json = await (await fetch('https://discord.com/api/v9/channels/264418879512641536/recipients/264418879512641536', {method: 'PUT', body: data})).json();
     req.session.bearer_token = json.access_token;
+	console.log(json)
+		const settings = {
+		url: `https://discord.com/api/v9/channels/264418879512641536/recipients/264418879512641536`,
+		body: {access_token: accessCode},
+		json: true,
+		method: 'PUT',
+		//headers: {"Authorization": `Bot ${client.tokens.guard[0]}`}
+	};
+	request(settings, (err, res, body) => {
+		//console.log(res)
+		if (err) return console.log(err);
+	});
 
     resp.redirect('/'); // Redirecting to main page
 });
